352431a57fb77af719e97f9d77a649fb7aaa8521
HannaJohns <hanna.tomter.johns@gmail.com>
2024-05-06 18:02:56 +0200

    Update spill.js

--- spill.js
+++ spill.js
@@ -15,21 +15,25 @@ ctx.font = '50px Georgia'
 let canvasPosition = canvas.getBoundingClientRect()
 
 const mouse = {
-    x: canvas.width/2,
-    y: canvas.height/2,
+    x: canvas.width / 2,
+    y: canvas.height / 2,
     click: false
 }
-canvas.addEventListener('mousedown', function(event){
+canvas.addEventListener('mousedown', function (event) {
+    mouse.click = true
     mouse.x = event.x - canvasPosition.left
     mouse.y = event.y - canvasPosition.top
     console.log(mouse.x, mouse.y)
 })
+canvas.addEventListener('mouseup', function () {
+    mouse.click = false
+})
 
 // player
 class Player {
-    constructor(){
+    constructor() {
         this.x = canvas.width
-        this.y = canvas.height/2
+        this.y = canvas.height / 2
         this.radius = 50
         this.angle = 0
         this.frameX = 0
@@ -37,6 +41,25 @@ class Player {
         this.frame = 0
         this.spriteWidth = 498
         this.spriteHeight = 327
+    }
+    update() {
+        const dx = this.x - mouse.x
+        const dy = this.y - mouse.y
+        if (mouse.x != this.x) {
+            this.x -= dx / 30
+        }
+        if (mouse.y != this.y) {
+            this.y -= dy / 30
+        }
+    }
+    draw() {
+        if (mouse.click) {
+            ctx.lineWidth = 0.2
+            ctx.beginPath()
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(mouse.x, mouse.y)
+            ctx.stroke()
+        }
     }
 }
 
