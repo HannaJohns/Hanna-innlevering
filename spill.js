2d25bb74e67ec4bf23384c43d3b363ec1c9b8928
HannaJohns <hanna.tomter.johns@gmail.com>
2024-05-29 21:56:12 +0200

    fiende
    fiende og game over

--- spill.js
+++ spill.js
@@ -11,6 +11,7 @@ let score = 0
 let gameFrame = 0
 ctx.font = '50px Georgia'
 let gameSpeed = 1
+let gameOver = false
 
 // mouse interactivity
 let canvasPosition = canvas.getBoundingClientRect()
@@ -24,7 +25,7 @@ canvas.addEventListener('mousedown', function (event) {
     mouse.click = true
     mouse.x = event.x - canvasPosition.left
     mouse.y = event.y - canvasPosition.top
-    console.log(mouse.x, mouse.y)
+    //console.log(mouse.x, mouse.y)
 })
 canvas.addEventListener('mouseup', function () {
     mouse.click = false
@@ -67,12 +68,12 @@ class Player {
             //ctx.lineTo(mouse.x, mouse.y)
             ctx.stroke()
         }
-        ctx.fillStyle = 'red'
+        /*ctx.fillStyle = 'red'
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
         ctx.fill()
         ctx.closePath()
-        // ctx.fillRect
+        // ctx.fillRect*/
 
         ctx.save()
         ctx.translate(this.x, this.y)
@@ -195,12 +196,12 @@ class Enemy{
         this.spriteHeight = 397
     }
     draw (){
-        ctx.fillStyle = 'red'
+        /*ctx.fillStyle = 'red'
         ctx.beginPath()
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
-        ctx.fill()
-        ctx.drawImage(enemyImage, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight,
-            this.spriteWidth, this.spriteHeight, this.x, this.y, this.radius * 2, this.radius * 2)
+        ctx.fill()*/
+        ctx.drawImage(enemyImage, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, 
+            this.spriteHeight, this.x - 60, this.y - 70, this.spriteWidth / 3, this.spriteHeight / 3)
     }
     update(){
         this.x -= this.speed
@@ -209,12 +210,38 @@ class Enemy{
             this.y = Math.random() * (canvas.height - 150) + 90
             this.speed = Math.random() * 2 + 2
         }
+        if (gameFrame % 5 == 0) {
+            this.frame++
+            if (this.frame >= 12) this.frame = 0
+            if (this.frame == 3 || this.frame == 7 || this.frame == 11){
+                this.frameX = 0
+            } else {
+                this.frameX++
+            }
+            if (this.frame < 3) this.frameY = 0
+            else if (this.frame < 7) this.frameY = 1
+            else if (this.frame < 11) this.frameY = 2
+            else this.frameY = 0
+        }
+        // kollisjon med spiller
+        const dx = this.x - player.x
+        const dy = this.y - player.y
+        const distance = Math.sqrt(dx * dx + dy * dy)
+        if (distance < this.radius + player.radius){
+            handleGameOver()
+        }
     }
 }
 const enemy1 = new Enemy()
 function handleEnemies(){
-    enemy1.update()
     enemy1.draw()
+    enemy1.update()
+}
+
+function handleGameOver(){
+    ctx.fillStyle = 'white'
+    ctx.fillText('GAME OVER, du nådde scoren ' + score, 110, 250)
+    gameOver = true
 }
 
 // animation loop
@@ -224,12 +251,12 @@ function animate() {
     handleBubbles()
     player.update()
     player.draw()
-    handleEnemies
+    handleEnemies()
     ctx.fillStyle = 'black'
     ctx.fillText('score: ' + score, 10, 50)
     gameFrame += 1
     // console.log(gameFrame)
-    requestAnimationFrame(animate)
+    if (!gameOver) requestAnimationFrame(animate)
 }
 animate()
 
